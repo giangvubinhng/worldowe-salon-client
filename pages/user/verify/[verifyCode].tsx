36a754b312a7b51614b3c80afcba33eab29e11f5
Giang Vu Binh Nguyen <giangvubinhn@vt.edu>
2022-01-26 10:54:28 -0500

    added link to verify user after signing up

---
+++ pages/user/verify/[verifyCode].tsx
@@ -0,0 +1,25 @@
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const VerifyPage: NextPage = () => {
+  const router = useRouter();
+  const [verify, setVerify] = useState<string | null>(null);
+  const verifyUser = async (code: string | string[] | undefined) => {
+    const { data } = await axios.get(
+      `http://localhost:5000/api/user/verify/${code}`
+    );
+    setVerify(data);
+  };
+
+  useEffect(() => {
+    if (!router.isReady) return;
+    const code = router.query.verifyCode;
+    console.log(code);
+    verifyUser(code);
+  }, [router.isReady]);
+  return <div>{verify ? <h1>{verify}</h1> : <h1>Verifying....</h1>}</div>;
+};
+
+export default VerifyPage;
