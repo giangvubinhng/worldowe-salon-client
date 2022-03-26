105bf24ae0950b7f97fc53110898abb6616de5b3
Giang Nguyen <giangvubinhn@vt.edu>
2022-03-25 20:08:42 -0400

    checkpoint

--- pages/shop/[id].tsx
+++ pages/shop/[id].tsx
@@ -1,7 +1,30 @@
+import styles from '../../styles/ShopDetail.module.css';
+import axios from 'axios'
+import {useQuery} from "@apollo/client";
+import {GET_SHOPS} from "../../graphql/shopQueries";
+
 const ShopPage = () => {
-	return (<div>
-		Sup
-	</div>)
+	return (
+		<div className={styles.container}>
+			Sup
+		</div>
+	)
+
+}
+export async function getStaticProps() {
+}
+
+export async function getStaticPaths() {
+	const {loading, error, data} = useQuery(GET_SHOPS);
+	return {
+		paths: data.map((r: any) => {
+			return {
+				params: {
+					id: r.id
+				}
+			}
+		})
+	}
 
 }
 
