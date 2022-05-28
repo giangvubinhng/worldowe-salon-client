5e6353066156b3472bd4769eabf1635725c088de
Giang Nguyen <giangvubinhn@vt.edu>
2022-05-28 00:44:35 -0400

    seach filter now working

--- components/Shops.tsx
+++ components/Shops.tsx
@@ -1,20 +1,22 @@
-import React, {useEffect} from "react";
-import {useQuery} from "@apollo/client";
-import {GET_SHOPS} from "../graphql/shopQueries";
+import React from "react";
 import {NextPage} from "next";
 import Card from "../components/Card";
 import {IShopBody} from "../interfaces/IShop";
 
 interface props {
-	clicked: boolean;
-	searchQuery: string;
-	resetSearch: Function;
+	data: any;
+	loading: boolean;
+	error: any
 }
-const Shops: NextPage<props> = ({clicked, searchQuery, resetSearch}) => {
-	const {loading, error, data} = useQuery(GET_SHOPS, {skip: !clicked, fetchPolicy: 'cache-and-network', variables: {name: searchQuery}});
+const Shops: NextPage<props> = ({data, loading, error}) => {
 
 	return (
 		<div>
+			{loading
+				? (<p>loading...</p>) : null}
+			{error
+				? (<p>Error</p>) : null}
+
 			{data
 				? data.shops.map((shop: IShopBody) => (
 					<Card shop={shop} key={shop.id} />
