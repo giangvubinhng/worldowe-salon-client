8220052b88a46a2c8296bd7d236197bcfd336f3a
Bao Ho <giabaoho123@gmail.com>
2022-06-09 22:10:27 -0700

    put photo into card

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from 'next';
-import { Card as BCard, Button} from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
+import { Card as BCard, Button } from 'react-bootstrap';
 import { IShopBody } from '../interfaces/IShop';
 import styles from '../styles/Card.module.css';
 
@@ -13,12 +14,25 @@ const Card: NextPage<props> = ({ shop }) => {
 		<div>
 			<BCard className={styles.Bcard}>
 				<BCard.Body>
-					<BCard.Title>{shop.shop_name}</BCard.Title>
-					<BCard.Text>
-						{shop.street}, {shop.city}, {shop.state}, {shop.zip}, {shop.country}
-					</BCard.Text>
-					<a target="_blank" href={`/shops/${shop.id}`} rel="noopener noreferrer">
-						<Button className={styles.Button}>Go To Shop</Button></a>
+					<div className={styles.flexboxContainer}>
+						
+						<div className={styles.leftBox}>
+							{/* <style jsx>{`
+        					.leftBox{
+          						display: list-items;
+        					}
+      						`}</style> */}
+							<BCard.Title>{shop.shop_name}</BCard.Title>
+							<BCard.Text>
+								{shop.street}, {shop.city}, {shop.state}, {shop.zip}, {shop.country}
+							</BCard.Text>
+							<a target="_blank" href={`/shops/${shop.id}`} rel="noopener noreferrer">
+								<Button className={styles.Button}>Go To Shop</Button></a>
+						</div>
+						<div className={styles.photo}>
+							<img src="https://source.unsplash.com/user/c_v_r" alt="Free unsplash image" width={200} height={200}></img>
+						</div>
+					</div>
 				</BCard.Body>
 			</BCard>
 		</div>
