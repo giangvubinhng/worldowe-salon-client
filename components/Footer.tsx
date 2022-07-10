f02cc6ccebb92d203f09d6751006037f308933de
Bao Ho <giabaoho123@gmail.com>
2022-07-09 23:41:38 -0700

    fisnish design the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => (
 		<footer className={styles.footer}>
 			<div className="container">
 				<div className="row">
-					<div className="col-6 col-md-2">
+					<div className="col-8 col-md-3">
 						<div className="column">
 							<h4 className="mt-3">Discover</h4>
 							<ul>
@@ -18,7 +18,7 @@ const Footer = () => (
 							</ul>
 						</div>
 					</div>
-					<div className="col-6 col-md-2">
+					<div className="col-8 col-md-3">
 						<div className="column">
 							<h4 className="mt-3">Resources</h4>
 							<ul>
@@ -37,70 +37,59 @@ const Footer = () => (
 							</ul>
 						</div>
 					</div>
-					<div className="col-6 col-md-2">
+					<div className="col-8 col-md-3">
 						<div className="column">
 							<h4 className="mt-3">Bussiness</h4>
 							<ul>
-								<li className="nav-item">
-									<a href="javascript:;">Support</a>
-								</li>
-								<li className="nav-item">
-									<a href="javascript:;">Jobs</a>
-								</li>
-								<li className="nav-item">
-									<a href="javascript:;">Privacy</a>
-								</li>
-								<li className="nav-item">
-									<a href="javascript:;">Guides</a>
-								</li>
-								<li className="nav-item">
-									<a href="javascript:;">Pricing</a>
-								</li>
+								<li><a href="#">Web design</a></li>
+								<li><a href="#">Development</a></li>
+								<li><a href="#">Hosting</a></li>
 							</ul>
 						</div>
 					</div>
-					<div className="col-md-4">
+					<div className="col-8 col-md-3">
 						<div className="column">
-							<h4 className="mt-3">Follow us</h4>
+							<div className="mt-3">
+								<label className={styles.item}>
+									<h5>Languages</h5>
+									<select>
+										<option>English</option>
+										<option>Spanish</option>
+										<option>Vietnamese</option>
+									</select>
+								</label>
+							</div>
+							<div className="mt-3">
+								<label className={styles.item}>
+									<h5>Country</h5>
+									<select>
+										<option>United States</option>
+										<option>Canada</option>
+										<option>Mexico</option>
+										<option>Vietnam</option>
+									</select>
+								</label>
+							</div>
 							<div className="btn-wrapper profile text-left">
-								<a href="https://twitter.com/creativetim" id="tooltip799285778" className="btn btn-twitter btn-sm"></a>
-								<a href="https://www.facebook.com/creativetim" id="tooltip33856223" target="_blank" className="btn btn-facebook btn-sm"></a>
-								<a href="https://dribbble.com/creativetim" id="tooltip521847749" target="_blank" className="btn btn-slack btn-sm"><i className="fab fa-slack"></i></a>
+								<ul>
+									<a href="https://twitter.com" id="tooltip799285778" className="btn btn-twitter btn-sm"></a>
+									<a href="https://www.facebook.com" id="tooltip33856223" className="btn btn-facebook btn-sm"></a>
+									<a href="https://dribbble.com" id="tooltip521847749" className="btn btn-slack btn-sm"><i className="fa fa-slack"></i></a>
+								</ul>
 							</div>
 						</div>
-						<div className="dropdown">
-							<label>
-								<h5>Languages</h5>
-								<select>
-									<option>English</option>
-									<option>Spanish</option>
-									<option>Vietnamese</option>
-								</select>
-							</label>
-						</div>
-						<div className="dropdown">
-							<label>
-								<h5>Country</h5>
-								<select>
-									<option>United States</option>
-									<option>Canada</option>
-									<option>Mexico</option>
-									<option>Vietnam</option>
-								</select>
-							</label>
-						</div>
-						<a
-							href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							Worldowe{' '}
-							<span className={styles.logo}>
-								<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-							</span>
-						</a>
 					</div>
 				</div>
+				<a className={styles.copyright}
+					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Worldowe{' '}
+					<span className={styles.logo}>
+						<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+					</span>
+				</a>
 			</div>
 		</footer>
 	</div>
