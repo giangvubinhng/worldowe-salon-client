562b12aeab0ddf66a7104f4832fda411a5aad252
Giang Nguyen <giangvubinhn@vt.edu>
2022-07-10 21:26:49 -0400

    checkpoint

--- components/Card.tsx
+++ components/Card.tsx
@@ -5,32 +5,59 @@ import { IShopBody } from '@/interfaces/IShop';
 import styles from '@/styles/Card.module.css';
 
 interface props {
-	shop: IShopBody;
+	shop?: IShopBody;
+	link?: string;
+	title?: string;
+	description?: string;
+	photo? : string;
 }
 
-const Card: NextPage<props> = ({ shop }) => {
-
+const Card: NextPage<props> = ({ shop, title, description, photo }) => {
+	if(shop){
+		return (
+			<div>
+				<a target="_blank" href={`/shops/${shop.id}`} rel="noopener noreferrer" className={styles.link}>
+					<BCard className={styles.Bcard}>
+						<BCard.Body>
+							<div className={styles.flexboxContainer}>
+								<div className={styles.leftBox}>
+									<BCard.Title className={styles.title}>{shop.shop_name}</BCard.Title>
+									<BCard.Text className={styles.info}>
+										{shop.street}, {shop.city}, {shop.state}, {shop.zip}, {shop.country}
+									</BCard.Text>
+								</div>
+								<div className={styles.photo}>
+									<img src="https://img.freepik.com/premium-vector/beauty-salon-color-illustration-hair-stylist-workplace-room-make-up-artist-barbershop-table-cosmetology-parlor-cartoon-interior-with-mirrors-armchairs-background_151150-1303.jpg?w=2000" alt="Free unsplash image" className={styles.CardPhoto}></img>
+								</div>
+							</div>
+						</BCard.Body>
+					</BCard>
+				</a>
+			</div>
+		);
+	}
 	return (
-		<div>
-			<a target="_blank" href={`/shops/${shop.id}`} rel="noopener noreferrer" className={styles.link}>
-				<BCard className={styles.Bcard}>
-					<BCard.Body>
-						<div className={styles.flexboxContainer}>
-							<div className={styles.leftBox}>
-								<BCard.Title className={styles.title}>{shop.shop_name}</BCard.Title>
-								<BCard.Text className={styles.info}>
-									{shop.street}, {shop.city}, {shop.state}, {shop.zip}, {shop.country}
-								</BCard.Text>
+			<div>
+					<BCard className={styles.Bcard}>
+						<BCard.Body>
+							<div className={styles.flexboxContainer}>
+								<div className={styles.leftBox}>
+									<BCard.Title className={styles.title}>{title}</BCard.Title>
+									<BCard.Text className={styles.info}>
+										{description}
+									</BCard.Text>
+								</div>
+								{photo ? 
+									(<div className={styles.photo}>
+										<img src={photo} alt="Free unsplash image" className={styles.PersonPhoto}></img>
+									</div>) : null
+								}
 							</div>
-							<div className={styles.photo}>
-								<img src="https://img.freepik.com/premium-vector/beauty-salon-color-illustration-hair-stylist-workplace-room-make-up-artist-barbershop-table-cosmetology-parlor-cartoon-interior-with-mirrors-armchairs-background_151150-1303.jpg?w=2000" alt="Free unsplash image" className={styles.CardPhoto}></img>
-							</div>
-						</div>
-					</BCard.Body>
-				</BCard>
-			</a>
-		</div>
-	);
+						</BCard.Body>
+					</BCard>
+			</div>
+)
+
 };
 
 export default Card;
