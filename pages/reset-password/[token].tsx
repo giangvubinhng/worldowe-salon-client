18bfc2906c2a09b7cb87b823aac092c462be7e6f
BaoHoHo <96350422+BaoHoHo@users.noreply.github.com>
2022-07-13 15:34:59 -0400

    check-point

--- pages/reset-password/[token].tsx
+++ pages/reset-password/[token].tsx
@@ -1,33 +1,95 @@
+import React, { useState } from 'react';
 import { changePassword } from '@/services/user.service';
-
+import { AuthorizedRoute } from '@/components/HOC/AuthorizedRoute';
+import { IReturnObject } from '@/interfaces/IBasicReturnTypeObject';
+import { GetServerSideProps } from 'next';
+import styles from '@/styles/Change-Password.module.css'
 
 const PasswordChange = () => {
-};
-return (
-    <div className={styles.container}>
-        <div className={styles.inner}>
-            <form onSubmit={submit}>
-                <div>
-                    <h2>Password Reset</h2>
-                    <p>Set a new password</p>
-                </div>
-                <div>
-                    <label>Old Password*</label>
-                    <input type="password" name="oldPassword" placeholder="Enter Old Password" value={input.oldPassword} onChange={onInputChange} style={{ width: "500px" }}></input>
-                </div>
-                <div>
-                    <label>New Password*</label>
-                    <input type="password" name="password" placeholder="Enter New Password" value={input.password} onChange={onInputChange} style={{ width: "500px" }}></input>
-                </div>
-                <div>
-                    <label>New Password Confirmation*</label>
-                    <input type="password" name="confirmPassword" placeholder="Confirm New Password" value={input.confirmPassword} onChange={onInputChange} style={{ width: "500px" }}></input>
-                </div>
-                <button className='btn btn-dark'>Done</button>
-            </form>
-            {error.hasError ? (<p>{error.message}</p>) : null}
-            {result.loaded ? (<p>{result.message}</p>) : null}
-        </div>
-    </div>
 
-);
+	let InitialReturnObject: IReturnObject = {
+		success: false,
+		message: '',
+		loaded: false
+	}
+
+	const [input, setInput] = useState({
+		oldPassword: '',
+		password: '',
+		confirmPassword: ''
+	});
+
+	const [error, setError] = useState({
+		hasError: false,
+		message: ''
+	});
+
+	const [result, setResult] = useState(InitialReturnObject)
+
+	const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = e.target;
+		setInput(prev => ({
+			...prev,
+			[name]: value
+		}));
+	}
+
+	const submit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		if (input.password != input.confirmPassword) {
+			setError({
+				hasError: true,
+				message: 'New passwords do not match'
+			})
+		}
+		else {
+			const result = await changePassword(input.oldPassword, input.password);
+			setError({
+				hasError: false,
+				message: ''
+			})
+			result.loaded = true;
+			setResult(result);
+		}
+	};
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.inner}>
+				<form onSubmit={submit}>
+					<div>
+						<h2>Password Reset</h2>
+						<p>Set a new password</p>
+					</div>
+					<div>
+						<label>Old Password*</label>
+						<input type="password" name="oldPassword" placeholder="Enter Old Password" value={input.oldPassword} onChange={onInputChange} style={{ width: "500px" }}></input>
+					</div>
+					<div>
+						<label>New Password*</label>
+						<input type="password" name="password" placeholder="Enter New Password" value={input.password} onChange={onInputChange} style={{ width: "500px" }}></input>
+					</div>
+					<div>
+						<label>New Password Confirmation*</label>
+						<input type="password" name="confirmPassword" placeholder="Confirm New Password" value={input.confirmPassword} onChange={onInputChange} style={{ width: "500px" }}></input>
+					</div>
+					<button className='btn btn-dark'>Done</button>
+				</form>
+				{error.hasError ? (<p>{error.message}</p>) : null}
+				{result.loaded ? (<p>{result.message}</p>) : null}
+			</div>
+		</div>
+
+	);
+}
+export default PasswordChange;
+
+export const getServerSideProps: GetServerSideProps = AuthorizedRoute(
+	async (ctx) => {
+		return {
+			props: {
+
+			},
+		}
+	}
+)
