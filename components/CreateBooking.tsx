760cf5e59ed0e2e7bc82dabd442e5513def9bf75
BaoHoHo <96350422+BaoHoHo@users.noreply.github.com>
2022-08-03 21:49:50 -0700

    checkpoint

--- components/CreateBooking.tsx
+++ components/CreateBooking.tsx
@@ -2,6 +2,8 @@ import { NextPage } from "next";
 import { sendEmailForgetPassword } from '@/services/user.service';
 import { Modal } from 'react-bootstrap'
 import { useState } from 'react';
+import styles from '../styles/CreateBooking.module.css'
+import { Button } from "react-bootstrap/lib/InputGroup";
 
 interface props {
 	bookClicked: boolean
@@ -14,7 +16,14 @@ type ResultReponse = {
 
 const CreateBooking: NextPage<props> = ({ bookClicked, hideClicked }) => {
 	const [state, setState] = useState({
-		forgotEmail: ''
+		firstname: '',
+		lastname: '',
+		email: '',
+		phone: '',
+		date: '',
+		time: '',
+		staff: '',
+		message: ''
 	});
 	const [result, setResult] = useState({ success: false, message: '' })
 	const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -24,27 +33,38 @@ const CreateBooking: NextPage<props> = ({ bookClicked, hideClicked }) => {
 			[name]: value,
 		});
 	};
-	const sendBooking = async (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		try {
 
-			const currResult = await sendEmailForgetPassword(state.forgotEmail) as ResultReponse;
-			if (currResult) {
-				setResult(currResult);
-			}
-		}
-		catch (e: unknown) {
-			setResult({ success: false, message: "An error has occurred" });
-		}
-
-
-	}
 	return (
 		<>
-			<Modal size="lg" centered show={bookClicked} onHide={() => hideClicked(false)}>
+			<Modal size="xl" centered show={bookClicked} onHide={() => hideClicked(false)}>
 				<Modal.Body>
-					<p>Book here
-					</p>
+					<form id="lost-form">
+						<h4>Client Infomation</h4>
+						<div className={styles.column}>
+							<label>First Name</label>
+							<input type="input" className={`form-control ${styles.input}`} placeholder="Enter First Name" required></input>
+							<label>Last Name</label>
+							<input type="input" className={`form-control ${styles.input}`} placeholder="Enter Last Name" required></input>
+							<label>Email</label>
+							<input type="email" className={`form-control ${styles.input}`} placeholder="Enter Email" required></input>
+							<label>Phone</label>
+							<input type="tel" className={`form-control ${styles.input}`} placeholder="Enter Phone Number" required></input>
+						</div>
+						<h4>Appoinment Detail</h4>
+						<div className={styles.column}>
+							<label>Date</label>
+							<input type="date" className={`form-control ${styles.input}`} required></input>
+							<label>Time</label>
+							<input type="time" className={`form-control ${styles.input}`} required></input>
+							<label>Staff</label>
+							<select id="staffs" name="staffs" className="dropdown">
+								<option value="giang">No preference</option>
+							</select>
+							<label>Appoinment Note</label>
+							<input type="input" className={`form-control ${styles.input}`} placeholder="(Optional)"></input>
+						</div>
+						<button className={`btn btn-dark`}>Book an appoiment</button>
+					</form>
 				</Modal.Body>
 			</Modal>
 		</>
